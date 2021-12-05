3937b5fed0cd8b341d49a21584ba5bfd1c4e3592
Jake Lindop <J.lindop98@gmail.com>
2021-12-05 21:53:46 +0000

    DCRNG-4 #done #comment created the character Race API

---
+++ race_api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, Response
+from random import randint
+
+app = Flask(__name__)
+
+@app.route("/race", methods=["GET"])
+def ch_class():
+    races = ["Human","Dwarf","Elf","Orc","Half-Elf","Dragonborn"]
+    random = races[randint(0,5)]
+    return Response(random, mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
